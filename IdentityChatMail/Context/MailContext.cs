a4a9bc882da1adde79a3edc968d4f2fd75dfa8dc
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-04-30 03:50:49 +0300

    Add project files.

---
+++ IdentityChatMail/Context/MailContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityChatMail.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityChatMail.Context
+{
+    public class MailContext:IdentityDbContext<AppUser>//benim ozellestirdigim sinifi da al !
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MULUSOY\\SQLEXPRESS01;Initial Catalog=LessonTwoDb;integrated Security=true; trust server certificate=true");
+        }
+    }
+}

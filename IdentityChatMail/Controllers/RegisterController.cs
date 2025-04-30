b7876aa1382ce22444a4492d6885498f41a42e63
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-04-30 13:11:51 +0300

    finish One

--- IdentityChatMail/Controllers/RegisterController.cs
+++ IdentityChatMail/Controllers/RegisterController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityChatMail.Entities;
+using IdentityChatMail.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityChatMail.Controllers
 {
     public class RegisterController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult CreateUser()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(RegisterViewModel model)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                Email = model.Email,
+                UserName = model.UserName
+            };
+            var result=await _userManager.CreateAsync(appUser,model.Password);
+            if (result.Succeeded) 
+            {
+                return RedirectToAction("UserLogin","Login");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+                return View();
+            }
+
+            
         }
     }
 }
